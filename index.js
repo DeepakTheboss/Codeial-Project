9fb0f00a43a9d59c22340afee0fdfaf7b372e150
deepak <deepakjaiswar120@gmail.com>
2023-09-06 03:33:00 +0530

    3rd commit: added layouts

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const app  = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+// before loading the views to the browser we have to set the wrappper for different content 
+//i.e for home.ejs or userProfile.ejs (variable part), we need to set the layouts(how page is structured)
+app.use(expressLayouts);
 
 //use express  router
 app.use('/', require('./routes')); // it will pick default index.js route(inside the routes folder i.e index.js)
