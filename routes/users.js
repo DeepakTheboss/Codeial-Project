25d32a5e4900a6a0debc6cb3b7de91a545d55635
deepak <deepakjaiswar120@gmail.com>
2023-09-09 13:42:16 +0530

    rendered pages for signIn and signUp

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,9 @@ const router = express.Router(); // Express Router is a module
 const usersController = require('../controllers/users_controller');
 router.get('/profile', usersController.profile);
 
+/// route for sign up which u type in browser
+router.get('/sign-up', usersController.signUp);
+
+router.get('/sign-in', usersController.signIn);
+
 module.exports = router;
