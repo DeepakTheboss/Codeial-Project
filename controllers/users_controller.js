4633bf2c9be16eed6e538a14c2df89be27a46cc9
deepak <deepakjaiswar120@gmail.com>
2023-09-11 01:52:56 +0530

    created sign up page with validation

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user')
 module.exports.profile = function(req,res){
     return  res.render('userProfile', {
         title: "User Profile"
@@ -35,12 +36,83 @@ module.exports.signIn = function(req,res){
     //
   }
 
+ /* get the sign up data or creating new user
+ module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        console.log('your password and confirm password should be match');
+        // user will be redirected to same sign-up page if password/cnf pass does not match
+        return res.redirect('back'); 
+    }
+
+    // if user email is unique then only we will create the user in database 
+    // otherwise we will simply say user with the same email id is already exists 
+    // or we can redirect to same / sign-up page
+
+    User.findOne({email: req.body.email}, function(err , newUser){
+         if(err){
+            console.log('While finding the email we are getting error on sign-up page',err);
+            return res.redirect('/users/sign-up');
+            // or return res.redirect('back');
+         }
+
+ // if user email is unique then only we will create the user in database 
+         if(!newUser)  // this new user is not present in database
+         {
+            User.create(req.body, async function(err , createdUser){
+              if(err){
+                console.log('error while creating or saving the user in db', err);
+                return res.redirect('back'); // return the sign-up page
+              }
+              
+              console.log('user created successfully in database', createdUser);
+              return res.redirect('/users/sign-in');
+              
+            })
+         }
+// user with the same email id already exists in database so simply redirect to the sign-up page
+         else{
+              console.log('User with this email id already exists',newUser.email );
+              return res.redirect('back');  // sign-up page
+         }
+    })
+
+  */      
 
 
+
+//get the sign up data or creating new user  (above code is original)    
+module.exports.create = async function(req, res) {
+      try {
+        // user will be redirected to same sign-up page if password/cnf pass does not match
+          if (req.body.password != req.body.confirm_password) {
+              console.log('Your password and confirm password should match');
+              return res.redirect('back');
+          }
   
 
+    // if user email is unique then only we will create the user in database 
+    // otherwise we will simply say user with the same email id is already exists 
+    // or we can redirect to same / sign-up page
 
+    //finding user by email
+          const newUser = await User.findOne({ email: req.body.email });
+  
+          // if user email is unique then only we will create the user in database 
+          // this new user is not present in database
+          if (!newUser) { 
+              const creatingUserInDb = await User.create(req.body);
+              console.log('User created successfully in the database',creatingUserInDb);
+              return res.redirect('/users/sign-in');
 
-
-
-
+          }
+          // user with the same email id already exists in database so simply redirect to the sign-up page 
+          else {
+              console.log('User with this email id already exists', newUser.email);
+              return res.redirect('back');
+          }
+      } catch (err) {
+          console.error('An error occurred during user registration:', err);
+          return res.redirect('/users/sign-up');
+      }
+  };
+  
