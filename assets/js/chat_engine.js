a7191dc8b48217f00bd53ed15bca401f5804508d
deepak <deepakjaiswar120@gmail.com>
2023-11-26 20:54:11 +0530

    created room for chat for multiple users

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -4,7 +4,7 @@ class ChatEngine{
         this.userEmail = userEmail;
 
         //chat_engine.js or we will emit an connect event then chat_sockets.js will recive a connection.
-        // and this emits back that u are connected using connect event 
+        // and this(backend) emits back(acknolwdeged) that u are connected using connect event 
         // using line no. 17 'connection established'
         this.socket = io.connect('http://localhost:5000');
 
@@ -15,8 +15,28 @@ class ChatEngine{
     }
 
     connectionHandler(){
+        let self = this;  // this has been change inside the onconnect 
         this.socket.on('connect', function(){
             console.log(`connection established using sockets...!`);
+
+
+  // step-1   // once we are connected to socket ask to join room
+            // whenever i am sending a request to join a room along with that i am sending data
+            // data means with which user wants to send request(chat with) and which room you want to join
+            // 1. in starting i my self wants to join with my email id 
+            // 2. and i will join room name (codeial room)
+            // when this event will be emitted then this event will be received at chat_sockets.js(backend)
+            self.socket.emit('join_room', {
+                user_email: self.userEmail,
+                chatroom: 'codeial'
+            });
+
+
+ // step-5 // chatengine will detect that new user joined the chatroom with server socket           
+            self.socket.on('user_joined', function(data){
+                console.log('a user joined!', data);
+            })
+
         })
     }
 } 
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -13,8 +13,33 @@ module.exports.chatSockets =(socketServer)=>{
 
         socket.on('disconnect' , function(){
             console.log('socket disconnected..');
+        });
+
+// step-2 // on will detect event comes from the client and here at chatsocket we will join room
+          // with some requested data from client 
+          // now we want to join the socket(client) with socket_chat_room
+        socket.on('join_room' , function(data){
+            console.log('joining request received ', data);
+
+
+// step-3    // if the data with chatroom is available then socket(user) will join the chatroom
+            // if data.chatroom is not exist then new chatroom will be created and socket(user) will join the room
+            socket.join(data.chatroom);
+
+// step-4    // now whenever i will join the chatroom then all the socket(user) inside the chatroom will get
+            // notification that some new user join the room and visa varsa
+            // so we need to tell everyone (tell == emit)
+            // in chatroom emit the message that new user joined with his data 
+            //now at the frontend this should be visible or recived that user joined 
+
+
+            io.in(data.chatroom).emit('user_joined', data);
+
+            // lets code in the frontend the frontend chatengine will recieve the emit msg
+
         })
-    })
+
+    });
      
 }
 
